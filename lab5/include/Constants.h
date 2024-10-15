478fda08784468ea9f7f06164d0fcd50bd248df9
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-15 10:58:05 +0300

    feat: add Constants.

---
+++ lab5/include/Constants.h
@@ -0,0 +1,31 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants
+{
+	const int months = 12;
+	const int days = 30;
+	const int hours = 12;
+
+	const int replacement_cost_shaft = 400;
+	const int repair_cost_shaft = 300;
+	const int repair_time_shaft = 2;
+	const int service_life_shaft = 300;
+
+	const int replacement_cost_electric_motor = 1500;
+	const int repair_cost_electric_motor = 300;
+	const int repair_time_electric_motor = 1;
+	const int service_life_electric_motor = 400;
+
+	const int replacement_cost_control_panel = 500;
+	const int repair_cost_control_panel = 100;
+	const int repair_time_control_panel = 2;
+	const int service_life_control_panel = 200;
+
+	const int replacement_cost_cutting_head = 1200;
+	const int repair_cost_cutting_head = 250;
+	const int repair_time_cutting_head = 3;
+	const int service_life_cutting_head = 350;
+}
+
+#endif
