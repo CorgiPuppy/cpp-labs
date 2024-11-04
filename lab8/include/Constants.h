002ab2155a3b8cc37fbdd9d24092905f4f9ec706
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-11-04 22:54:42 +0300

    feat: add namespace for all constants of the lab.

---
+++ lab8/include/Constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants
+{
+	const int title_size = 100;
+	const int author_size = 100;
+	const int genre = 30;
+	const int max_amount_of_ratings = 100;
+
+	const int max_amount_of_books = 30;
+}
+
+#endif
+
