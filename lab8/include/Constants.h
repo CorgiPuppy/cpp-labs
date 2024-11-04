c9f0cf6394a4e16b98446952a6776caf211083f9
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-11-04 23:35:39 +0300

    fix: modify genre_size.

--- lab8/include/Constants.h
+++ lab8/include/Constants.h
@@ -5,7 +5,7 @@ namespace Constants
 {
 	const int title_size = 100;
 	const int author_size = 100;
-	const int genre = 30;
+	const int genre_size = 50;
 	const int max_amount_of_ratings = 100;
 
 	const int max_amount_of_books = 30;
