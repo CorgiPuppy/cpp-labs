33756f0614ab2e7609e593bbf771a7b73493b79f
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-11-05 01:27:20 +0300

    fix: add constants for simulating.

--- lab8/include/Constants.h
+++ lab8/include/Constants.h
@@ -9,6 +9,26 @@ namespace Constants
 	const int max_amount_of_ratings = 100;
 
 	const int max_amount_of_books = 30;
+
+	const int max_age_of_visitors = 100;
+	const int min_age_of_visitors = 10;
+
+	const int amount_of_genres = 6;
+	const char* genre_names[amount_of_genres] = {"Научная фантастика", "Фэнтези", "Исторический роман", "Детектив", "Роман", "Приключения"};
+
+	const int amount_of_preferred_volumes = 3;
+
+	const int max_reading_speed = 100;
+	const int min_reading_speed = 10;
+
+	const int the_first_day = 1;
+	const int amount_of_days = 30;
+
+	const int max_amount_of_visitors = 5; 
+	const int min_amount_of_visitors = 1;
+
+	const int max_rating = 10;
+	const int min_rating = 1;
 }
 
 #endif
