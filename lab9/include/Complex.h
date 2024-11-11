022e7c67844d602ae4f61140bec0e2f9adfdf561
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-11-11 13:28:59 +0300

    feat: add class with methods and getters.

---
+++ lab9/include/Complex.h
@@ -0,0 +1,57 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <cmath>
+
+class Complex
+{
+	public:
+		double real;
+		double imag;
+
+	public:
+		Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
+
+		Complex operator+(const Complex& second)
+		{
+			return Complex(real + second.real, imag + second.imag);
+		}
+
+		Complex operator-(const Complex& second)
+		{
+			return Complex(real - second.real, imag - second.imag);
+		}
+
+		Complex operator*(const Complex& second)
+		{
+			return Complex(real * second.real - imag * second.imag,
+                           real * second.imag + imag * second.real);
+        }
+
+		Complex operator/(const Complex& second)
+		{
+			double denominator = (second.real * second.real + second.imag * second.imag);	
+
+			return Complex((real * second.real + imag * second.imag) / denominator,
+                           (imag * second.real - real * second.imag) / denominator);
+		}
+
+		Complex sqrt()
+		{
+			double r = std::sqrt(std::sqrt(real * real + imag * imag));
+			double theta = 0.5 * std::atan2(imag, real);
+			return Complex(r * std::cos(theta), r * std::sin(theta));
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const Complex& c)
+		{
+			if (c.imag >= 0)
+				os << c.real << " + " << c.imag << "i";
+			else
+				os << c.real << " - " << -c.imag << "i";
+			
+			return os;
+		}
+};
+
+#endif
---
+++ lab9/include/QuadraticEquation.h
@@ -0,0 +1,54 @@
+#ifndef QUADRATIC_EQUATION_H
+#define QUADRATIC_EQUATION_H
+
+#include "Complex.h"
+
+class QuadraticEquation
+{
+	private:
+		Complex a, b, c;
+		Complex root_1, root_2;
+
+		void calculate_roots()
+		{
+	        Complex discriminant = b * b - Complex(4.0, 0.0) * a * c; 
+            
+			root_1 = (Complex(0.0, 0.0) - b + discriminant.sqrt()) / (Complex(2.0, 0.0) * a);
+			root_2 = (Complex(0.0, 0.0) - b - discriminant.sqrt()) / (Complex(2.0, 0.0) * a);
+		}
+
+	public:
+		QuadraticEquation
+		(
+			Complex a,
+			Complex b,
+			Complex c
+		) :
+			a(a),
+			b(b),
+			c(c)
+		{
+			calculate_roots();
+		}
+
+		bool has_root(const Complex& number) const 
+		{
+			return (root_1.real == number.real && root_1.imag == number.imag) ||
+                   (root_2.real == number.real && root_2.imag == number.imag);
+		}
+
+		const Complex get_root_1() const { return root_1; }
+
+		const Complex get_root_2() const { return root_2; }
+
+		friend std::ostream& operator<<(std::ostream& os, const QuadraticEquation& qe)
+		{
+			os << std::endl << "\tУравнение: " << "(" << qe.a << ")" << "x^2 + " << "(" << qe.b << ")" << "x + " << "(" << qe.c << ") = 0" << std::endl;
+			os << "\t\tКорень 1: " << qe.root_1 << std::endl;
+			os << "\t\tКорень 2: " << qe.root_2;
+
+			return os;
+		}
+};
+
+#endif
